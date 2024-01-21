902c8473c3293c30c4b17e57c2b1a3a1376d33f4
Hafiffdl <hafif.fdl16@gmail.com>
2024-01-21 22:26:30 +0700

    add: root color collection

---
+++ lib/constants/color_collection.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorCollection {
+  static const Color primary = Color(0xFF333365);
+  static const Color sideColor = Color(0xFF6666A2);
+  static const Color fillColor = Color(0xFFDEDEF6);
+}
