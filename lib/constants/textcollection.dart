aacb842c054d3104f29b3cdb935ed5c482d068d0
Hafiffdl <hafif.fdl16@gmail.com>
2024-01-22 17:38:34 +0700

    add: text collection

---
+++ lib/constants/textcollection.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class TextCollection {
+  static const TextStyle heading1 = TextStyle(
+    fontFamily: 'PlusJakartaSans',
+    fontWeight: FontWeight.w700,
+    fontSize: 26,
+    letterSpacing: 0,
+    color: Color(0xFF333365),
+  );
+
+  static const TextStyle heading2 = TextStyle(
+    fontFamily: 'PlusJakartaSans',
+    fontWeight: FontWeight.w500,
+    fontSize: 18,
+    height: 0.06,
+    color: Colors.white,
+  );
+
+  static const TextStyle bodytext = TextStyle(
+    fontFamily: 'PlusJakartaSans',
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+    letterSpacing: 0,
+    color: Color(0xFF1E1E1E),
+  );
+
+  static const TextStyle buttontext = TextStyle(
+    fontFamily: 'PlusJakartaSans',
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+    letterSpacing: 0,
+    color: Colors.white,
+  );
+
+  static const TextStyle buttontext1 = TextStyle(
+    fontFamily: 'PlusJakartaSans',
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+    letterSpacing: 0,
+    color: Color(0xFF6666A2),
+  );
+
+  static const TextStyle logintext = TextStyle(
+    fontFamily: 'PlusJakartaSans',
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+    letterSpacing: 0,
+    color: Color(0xFF333365),
+  );
+
+  static const TextStyle bodylogin = TextStyle(
+    fontFamily: 'PlusJakartaSans',
+    fontWeight: FontWeight.w500,
+    fontSize: 18,
+    color: Color(0xFF1E1E1E),
+  );
+
+  static const TextStyle loginbuttontext = TextStyle(
+    fontFamily: 'PlusJakartaSans',
+    fontWeight: FontWeight.w500,
+    fontSize: 13,
+    color: Color(0xFF333365),
+    decoration: TextDecoration.underline,
+    height: 0.08,
+  );
+
+  static const TextStyle signupbuttontext = TextStyle(
+    fontFamily: 'PlusJakartaSans',
+    fontWeight: FontWeight.w400,
+    fontSize: 12,
+    height: 0.08,
+    color: Color(0xFF333365),
+  );
+
+  static const TextStyle signupbuttontext1 = TextStyle(
+    fontFamily: 'PlusJakartaSans',
+    fontWeight: FontWeight.w700,
+    fontSize: 12,
+    height: 0.08,
+    color: Color(0xFF333365),
+  );
+
+  static const TextStyle bodylogin1 = TextStyle(
+    fontFamily: 'PlusJakartaSans',
+    fontWeight: FontWeight.w500,
+    fontSize: 18,
+    color: Color(0xFF333365),
+  );
+
+}
